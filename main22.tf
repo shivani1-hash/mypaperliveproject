553f71f04d409781e5c0516b4e25cbe7ca17eed0
Shivani Sharma <shivanisharma@Shivanis-MacBook-Air.local>
2025-05-24 18:08:29 +0530

    add this file

---
+++ main22.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  }
+}
