2febe83abc33bba674955bb1f1b2d8c2d7a5baf5
shivani1-hash <84497699+shivani1-hash@users.noreply.github.com>
2025-05-24 20:03:07 +0530

    Update main22.tf
    yes pls update

--- main22.tf
+++ main22.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
-
 resource "aws_instance" "example_server" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
